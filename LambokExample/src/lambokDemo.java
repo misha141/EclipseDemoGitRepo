0a01be7227611b335dcb51cb23fa73adbdf2ee94
Misha Kumari <mishasingh141@gmail.com>
2021-07-08 17:53:55 +0530

    First Commit

---
+++ LambokExample/src/lambokDemo.java
@@ -0,0 +1,8 @@
+
+public class lambokDemo {
+
+	public static void main(String[] args) {
+		modelClass m = new modelClass();
+
+	}
+}
---
+++ LambokExample/src/modelClass.java
@@ -0,0 +1,13 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class  modelClass {
+	
+	private int id;
+	private int age;
+	private String name;
+	
+}
